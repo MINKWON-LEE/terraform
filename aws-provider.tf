e2b9455d935af9d020448756dc51c7197588a26f
MINKWON-LEE <kaisermk21@naver.com>
2023-12-22 10:30:22 +0900

    Modified file

--- aws-provider.tf
+++ aws-provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAV6FPLAQE4YCV3IYS"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "ap-northeast-2"
 }
